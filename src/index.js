da7eb5d4b64c207058a211be1ca4df5994b75e2b
Muhammad Bilal Bin Khalid <bilalbink1@gmail.com>
2021-04-18 22:46:03 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import UserPanel from './pages/user-panel/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UserPanel />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/pages/user-panel/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserPanel() {
+    return (
+        <div>start</div>
+    )
+}
+
+export default UserPanel;
