e9286327821fcc3fd82476e3c18ea2e84e9f50bd
Muhammad Bilal Bin Khalid <bilalbink1@gmail.com>
2021-04-19 03:20:52 +0300

    added sidebar

--- src/pages/user-panel/sidebar/index.js
+++ src/pages/user-panel/sidebar/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FeatherIcon from 'feather-icons-react'
 import './styles.modules.css'
 
 function Sidebar() {
+    const [active, setActive] = useState(-1);
+    const iconList = ['home', 'mail', 'user', 'settings', 'bar-chart-2']
+
+    function selectPage(index) {
+        setActive(index);
+    }
+
     return (
-        <div className='container'>as</div>
+        <div className='container'>
+            {iconList.map((icon, index) => {
+                return (
+                    <div key={index} className={index == active ? 'active-icon-container' : 'icon-container'} onClick={() => selectPage(index)}>
+                        <FeatherIcon  className={index == active ? 'active-icon' : 'sidebar-icon'} size={40} icon={icon}/>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
